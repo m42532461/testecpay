e47c85190bca96c8e85fcf0848ed436986e5013b
m42532461 <m42532461@gmail.com>
2022-10-26 01:34:24 +0800

    add some layout

--- src/components/Deal.jsx
+++ src/components/Deal.jsx
@@ -2,26 +2,37 @@ import React from "react";
 
 const Deal = () => {
   return (
-    <div className="flex justify-between">
-      <div className="">
-        <h1>Hot deals for you</h1>
-        <span>Online shopping for retail sales direct to consumers</span>
+    <div className="flex justify-between px-24 text-start gap-[65px] pt-[138px] pb-[109px]">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-[50px] leading-[50px]">
+          Hot <br />
+          deals for you
+        </h1>
+        <p className="text-[16px] leading-[26px] max-w-[233px]">
+          Online shopping for retail sales direct to consumers
+        </p>
       </div>
-      <div className="flex">
-        <div className="">
+      <div className="flex gap-[120px]">
+        <div className="flex flex-col gap-6 max-w-[188px]">
           icon
-          <h1>1.5% cashback</h1>
-          <span>Online shopping for retail sales direct to consumers</span>
+          <h1 className="text-[20px] leading-[28px]">1.5% cashback</h1>
+          <span className="text-[16px] leading-[26px]">
+            Online shopping for retail sales direct to consumers
+          </span>
         </div>
-        <div className="">
+        <div className="flex flex-col gap-6 max-w-[188px]">
           icon
-          <h1>30-day terms</h1>
-          <span>Online shopping for retail sales direct to consumers</span>
+          <h1 className="text-[20px] leading-[28px]">30-day terms</h1>
+          <span className="text-[16px] leading-[26px]">
+            Online shopping for retail sales direct to consumers
+          </span>
         </div>
-        <div className="">
+        <div className="flex flex-col gap-6 max-w-[188px]">
           icon
-          <h1>Save Money</h1>
-          <span>Online shopping for retail sales direct to consumers</span>
+          <h1 className="text-[20px] leading-[28px]">Save Money</h1>
+          <span className="text-[16px] leading-[26px]">
+            Online shopping for retail sales direct to consumers
+          </span>
         </div>
       </div>
     </div>
