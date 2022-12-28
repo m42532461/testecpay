39a27df606bcce6e4c4a50724854f2839f101658
m42532461 <m42532461@gmail.com>
2022-12-28 19:49:54 +0800

    Update baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecpay-production.up.railway.app",
+  baseURL: "https://forfurnishop-production.up.railway.app",
 });
 
 API.interceptors.request.use((req) => {
